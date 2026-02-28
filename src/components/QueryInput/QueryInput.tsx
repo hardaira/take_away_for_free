a1e38389b1f5352459f16a82a68fd8f0ecf73f53
hardaira <hardaira@yahoo.com>
2026-02-28 13:46:29 +0100

    add solution

--- src/components/QueryInput/QueryInput.tsx
+++ src/components/QueryInput/QueryInput.tsx
@@ -1,20 +1,28 @@
 import { SlMagnifier } from 'react-icons/sl';
-import React, { useState } from 'react';
+import React, {
+  useState, useRef, useEffect
+} from 'react';
 import { useSearchParams } from "react-router-dom";
 import './QueryInput.scss';
 export const QueryInput: React.FC = () => {
   const [query, setQuery] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
+  // const inputRef = useRef<HTMLInputElement>(null);
 
   const storedCity = localStorage.getItem("activeCity");
 
   const activeCity = searchParams.get("city") || storedCity || "Вся Україна";
+
+  // useEffect(() => {
+  //   inputRef.current?.focus();
+  // }, []);
   return (
     <div className="list__heading">
       <p className="list__top">{activeCity}, обирай найкращі пропозиції!</p>
       <div className="input-wrapper">
         <SlMagnifier className="input-icon" />
         <input
+          // ref={inputRef}
           type="text"
           className="input-style query"
           placeholder="Шукай за назвою"
