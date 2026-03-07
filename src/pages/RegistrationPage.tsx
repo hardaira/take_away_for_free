14195a7aa1ef49057a2654da038b2029cfc565c0
hardaira <hardaira@yahoo.com>
2026-03-07 09:53:43 +0100

    registration

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -20,11 +20,14 @@ export const RegistrationPage: React.FC = () => {
     setLoading(true);
 
     try {
-      const res = await fetch('http://localhost:5000/users/registration', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, email, password }),
-      });
+      const res = await fetch(
+        "http://team-project-backend-production.up.railway.app/auth/registration",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name, email, password }),
+        }
+      );
 
       const data = await res.json();
       console.log(data);
@@ -35,6 +38,7 @@ export const RegistrationPage: React.FC = () => {
         setName('');
         setEmail('');
         setPassword('');
+        console.log("Data received!");
         return;
       }
 
@@ -44,7 +48,7 @@ export const RegistrationPage: React.FC = () => {
         return;
       }
 
-      setRegistrationError('Registration failed. Please try again later.');
+      //setRegistrationError('Registration failed. Please try again later.');
     } catch (error) {
       setRegistrationError('Реєстрація не пройшла. Сервер не відповідає.');
 
@@ -54,59 +58,67 @@ export const RegistrationPage: React.FC = () => {
   };
 
   return (
-    <div className="section">
-      <div className="container">
-        <div className="centered">
-          <div className="form__box__login">
-            <h2>Реєстрація</h2>
+    
+      <div className="section">
+        <div className="container">
+          <div className="centered">
+            <div className="form__box__login">
+              <h2>Реєстрація</h2>
 
-            {/* <form onSubmit={handleAddUser}> */}
-            <form className="registration__box" onSubmit={handleAddUser}>
-              {/* <label htmlFor="name">Введіть ім'я користувача</label>  */}
-              <div className="input-wrapper">
-                <input
-                  type="text"
-                  id="name"
-                  className="input-style"
-                  placeholder="Введіть своє ім'я"
-                  value={name}
-                  onChange={e => setName(e.target.value)}
-                />
-              </div>
-              {/* <label htmlFor="email">E-mail</label> */}
-              <div className="input-wrapper">
-                <input
-                  type="email"
-                  id="email"
-                  className="input-style"
-                  placeholder="Введіть свій email"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </div>
-              {/* <label htmlFor="password">Password(min 8 symbols)</label> */}
-              <div className="input-wrapper">
-                <input
-                  type="password"
-                  id="password"
-                  className="input-style"
-                  placeholder="Введіть свій пароль"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-              </div>
+              {/* <form onSubmit={handleAddUser}> */}
+              <form className="registration__box" onSubmit={handleAddUser}>
+                {/* <label htmlFor="name">Введіть ім'я користувача</label>  */}
+                <div className="input-wrapper">
+                  <input
+                    type="text"
+                    id="name"
+                    className="input-style"
+                    placeholder="Введіть своє ім'я"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                {/* <label htmlFor="email">E-mail</label> */}
+                <div className="input-wrapper">
+                  <input
+                    type="email"
+                    id="email"
+                    className="input-style"
+                    placeholder="Введіть свій email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                {/* <label htmlFor="password">Password(min 8 symbols)</label> */}
+                <div className="input-wrapper">
+                  <input
+                    type="password"
+                    id="password"
+                    className="input-style"
+                    placeholder="Введіть свій пароль"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
 
-              <button type="submit" className="loginButton" disabled={loading}>
-                {loading ? 'Надсилається' : 'Надіслати'}
-              </button>
-              {registrationError && (
-                <p style={{ color: 'red' }}>{registrationError}</p>
-              )}
-            </form>
+                <button
+                  type="submit"
+                  className="loginButton"
+                  disabled={loading}
+                >
+                  {loading ? "Надсилається" : "Надіслати"}
+                </button>
+                {registrationError && (
+                  <p style={{ color: "red" }}>{registrationError}</p>
+                )}
+              </form>
+            </div>
+            {successMessage && (
+              <p style={{ color: "green" }}>{successMessage}</p>
+            )}
           </div>
-          {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
         </div>
       </div>
-    </div>
+    
   );
 };
