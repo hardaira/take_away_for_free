25f01d90ee56013f2279f9bcdd2e929d716ce565
hardaira <hardaira@yahoo.com>
2026-03-24 10:50:58 +0100

    styled heart

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,5 @@
 import './ProductCard.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 // import { LuHeart } from 'react-icons/lu';
 // import { CiWallet } from 'react-icons/ci';
