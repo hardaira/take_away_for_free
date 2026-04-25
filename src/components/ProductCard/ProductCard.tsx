468e53c3bad2bfd09b9c46518e23a9c0eb3207ce
hardaira <hardaira@yahoo.com>
2026-04-25 19:40:25 +0200

    show details

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -300,9 +300,9 @@ export const ProductCard: React.FC<ProductCardProps> = ({
             <div
               className="addButton"
               onClick={() => {
-                navigate(`/product/${id}`);
+                //navigate(`/product/${id}`);
                 navigate(user ? `/product/${id}` : "/details");
-                setShowDetails(true);
+                //setShowDetails(true);
               }}
             >
               Повний опис
